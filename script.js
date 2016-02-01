78d160224f61574fe5804821b1755a383c7d6a1f
Jarrett Gingrich <jarrettgingrich@gmail.com>
2016-01-31 20:18:39 -0500

    Solved problem 2

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ for (i = 0; i < 1000; i = i + 5){
     byFive += i;
 }
 console.log(byThree + byFive);
+
+//Challenge 2
+var a = 0;
+var b = 1;
+var sum = 0;
+
+while( a < 4000000) {
+    
+    if( a % 2 === 0){
+        sum += a;
+    }
+    var c = a + b;
+    a = b;
+    b = c;
+}
+
+console.log(sum);
